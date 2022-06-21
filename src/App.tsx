656e1524350b1311103cbda044a6fe48af3ffa10
Noah Reiter <n.reiter@alpin11.com>
2022-06-21 13:33:11 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import { Box, Button, Container, Heading, Img, Link, Text } from '@chakra-ui/react'
+import { Box, Button, Container, Heading, Img, Link, LinkOverlay, Text, useProps } from '@chakra-ui/react'
 import React, { useState } from 'react'
+import { render } from 'react-dom'
 import { load } from 'react-require'
 import agerFarmer from './agerfarmer.png'
 import martinator from './martinator.png'
 import tie from './tie.png'
 
 const App = () => {
-  const [start, setStart] = useState(true)
+  let [start, setStart] = useState(true)
   const [turn, setTurn] = useState(false)
   const [firstBox, setFirstBox] = useState(false)
   const [secondBox, setSecondBox] = useState(false)
@@ -18,6 +19,36 @@ const App = () => {
   const [eighthBox, setEighthBox] = useState(false)
   const [ninthBox, setNinthBox] = useState(false)
   const [count, setCount] = useState(0)
+  const [reset, setReset] = useState(false)
+  const agerOrMartinator =
+  [
+    'o', 'o', 'o',
+    'o', 'o', 'o',
+    'o', 'o', 'o',
+  ]
+  console.log(agerOrMartinator)
+
+  if (firstBox || secondBox || thirdBox || fourthBox || fifthBox || sixthBox || seventhBox || eighthBox || ninthBox) {
+    start = false
+  }
+
+  if (reset) {
+    setFirstBox(false)
+    setSecondBox(false)
+    setThirdBox(false)
+    setFourthBox(false)
+    setFifthBox(false)
+    setSixthBox(false)
+    setSeventhBox(false)
+    setEighthBox(false)
+    setNinthBox(false)
+    setStart(true)
+    setCount(0)
+    setTurn(false)
+    setReset(false)
+  }
+
+  agerOrMartinator[0] = martinator
 
   const round = 'x'
   return (
@@ -34,43 +65,50 @@ const App = () => {
           <Text color="white">Round </Text>
           <Text color="#31C3BD">{round}</Text>
         </Box>
+        {agerOrMartinator.map((item, index) => {
+          return (
+            <Link onClick={() => { setFirstBox(true); setCount(count + 1) }} _hover={{ border: 'none' }} key={`${index}tikTakToeFeed`}>
+              {firstBox ? <Box borderRight="4px" borderBottom="4px" h="204px" w="263px" pt="1px"><Img src={agerOrMartinator[0]} h="150px" w="150px" ml="60px" mt="30px"/></Box> : <Box borderRight="4px" borderBottom="4px" p="25px 100px 25px 100px"><Text color="#1A2A33">o</Text></Box>}
+            </Link>
+          )
+        })}
         <Box w="600px" color="grey" display="flex" fontSize="100px" justifyContent="space-evenly" ml="auto" mr="auto" >
           <Box>
             <Link onClick={() => { setFirstBox(true); setCount(count + 1) }} _hover={{ border: 'none' }}>
-              {firstBox ? <Box borderRight="4px" borderBottom="4px" h="204px" w="263px" pt="1px"><Img src={agerFarmer} h="150px" w="150px" ml="60px" mt="30px"/></Box> : <Box borderRight="4px" borderBottom="4px" p="25px 100px 25px 100px"><Text color="#1A2A33">o</Text></Box>}
+              {firstBox ? <Box borderRight="4px" borderBottom="4px" h="204px" w="263px" pt="1px"><Img src={agerOrMartinator[0]} h="150px" w="150px" ml="60px" mt="30px"/></Box> : <Box borderRight="4px" borderBottom="4px" p="25px 100px 25px 100px"><Text color="#1A2A33">o</Text></Box>}
             </Link>
             <Link onClick={() => { setSecondBox(true); setCount(count + 1) }} _hover={{ border: 'none' }}>
-              {secondBox ? <Box borderRight="4px" borderBottom="4px" h="204px" w="263px" pt="1px"><Img src={agerFarmer} h="150px" w="150px" ml="60px" mt="30px"/></Box> : <Box borderRight="4px" borderBottom="4px" p="25px 100px 25px 100px"><Text color="#1A2A33">o</Text></Box>}
+              {secondBox ? <Box borderRight="4px" borderBottom="4px" h="204px" w="263px" pt="1px"><Img src={agerOrMartinator[3]} h="150px" w="150px" ml="60px" mt="30px"/></Box> : <Box borderRight="4px" borderBottom="4px" p="25px 100px 25px 100px"><Text color="#1A2A33">o</Text></Box>}
             </Link>
             <Link onClick={() => { setThirdBox(true); setCount(count + 1) }} _hover={{ border: 'none' }}>
-              {thirdBox ? <Box borderRight="4px" h="200px" w="263px" pt="1px"><Img src={agerFarmer} h="150px" w="150px" ml="60px" mt="30px"/></Box> : <Box borderRight="4px" p="25px 100px 25px 100px"><Text color="#1A2A33">o</Text></Box>}
+              {thirdBox ? <Box borderRight="4px" h="200px" w="263px" pt="1px"><Img src={agerOrMartinator[7]} h="150px" w="150px" ml="60px" mt="30px"/></Box> : <Box borderRight="4px" p="25px 100px 25px 100px"><Text color="#1A2A33">o</Text></Box>}
             </Link>
           </Box>
           <Box>
             <Link onClick={() => { setFourthBox(true); setCount(count + 1) }} _hover={{ border: 'none' }}>
-              {fourthBox ? <Box borderRight="4px" borderBottom="4px" h="204px" w="263px" pt="1px"><Img src={agerFarmer} h="150px" w="150px" ml="60px" mt="30px"/></Box> : <Box borderRight="4px" borderBottom="4px" p="25px 100px 25px 100px"><Text color="#1A2A33">o</Text></Box>}
+              {fourthBox ? <Box borderRight="4px" borderBottom="4px" h="204px" w="263px" pt="1px"><Img src={agerOrMartinator[1]} h="150px" w="150px" ml="60px" mt="30px"/></Box> : <Box borderRight="4px" borderBottom="4px" p="25px 100px 25px 100px"><Text color="#1A2A33">o</Text></Box>}
             </Link>
             <Link onClick={() => { setFifthBox(true); setCount(count + 1) }} _hover={{ border: 'none' }}>
-              {fifthBox ? <Box borderRight="4px" borderBottom="4px" h="204px" w="263px" pt="1px"><Img src={agerFarmer} h="150px" w="150px" ml="60px" mt="30px"/></Box> : <Box borderRight="4px" borderBottom="4px" p="25px 100px 25px 100px"><Text color="#1A2A33">o</Text></Box>}
+              {fifthBox ? <Box borderRight="4px" borderBottom="4px" h="204px" w="263px" pt="1px"><Img src={agerOrMartinator[4]} h="150px" w="150px" ml="60px" mt="30px"/></Box> : <Box borderRight="4px" borderBottom="4px" p="25px 100px 25px 100px"><Text color="#1A2A33">o</Text></Box>}
             </Link>
             <Link onClick={() => { setSixthBox(true); setCount(count + 1) }} _hover={{ border: 'none' }}>
-              {sixthBox ? <Box borderRight="4px" h="200px" w="263px" pt="1px"><Img src={agerFarmer} h="150px" w="150px" ml="60px" mt="30px"/></Box> : <Box borderRight="4px" p="25px 100px 25px 100px"><Text color="#1A2A33">o</Text></Box>}
+              {sixthBox ? <Box borderRight="4px" h="200px" w="263px" pt="1px"><Img src={agerOrMartinator[8]} h="150px" w="150px" ml="60px" mt="30px"/></Box> : <Box borderRight="4px" p="25px 100px 25px 100px"><Text color="#1A2A33">o</Text></Box>}
             </Link>
           </Box>
           <Box>
             <Link onClick={() => { setSeventhBox(true); setCount(count + 1) }} _hover={{ border: 'none' }}>
-              {seventhBox ? <Box borderBottom="4px" h="204px" w="259px" pt="1px"><Img src={agerFarmer} h="150px" w="150px" ml="60px" mt="30px"/></Box> : <Box borderBottom="4px" p="25px 100px 25px 100px"><Text color="#1A2A33">o</Text></Box>}
+              {seventhBox ? <Box borderBottom="4px" h="204px" w="259px" pt="1px"><Img src={agerOrMartinator[2]} h="150px" w="150px" ml="60px" mt="30px"/></Box> : <Box borderBottom="4px" p="25px 100px 25px 100px"><Text color="#1A2A33">o</Text></Box>}
             </Link>
             <Link onClick={() => { setEighthBox(true); setCount(count + 1) }} _hover={{ border: 'none' }}>
-              {eighthBox ? <Box borderBottom="4px" h="204px" w="259px" pt="1px"><Img src={agerFarmer} h="150px" w="150px" ml="60px" mt="30px"/></Box> : <Box borderBottom="4px" p="25px 100px 25px 100px"><Text color="#1A2A33">o</Text></Box>}
+              {eighthBox ? <Box borderBottom="4px" h="204px" w="259px" pt="1px"><Img src={agerOrMartinator[6]} h="150px" w="150px" ml="60px" mt="30px"/></Box> : <Box borderBottom="4px" p="25px 100px 25px 100px"><Text color="#1A2A33">o</Text></Box>}
             </Link>
             <Link onClick={() => { setNinthBox(true); setCount(count + 1) }} _hover={{ border: 'none' }}>
-              {ninthBox ? <Box h="200px" w="259px" pt="1px"><Img src={agerFarmer} h="150px" w="150px" ml="60px" mt="30px"/></Box> : <Box p="25px 100px 25px 100px"><Text color="#1A2A33">o</Text></Box>}
+              {ninthBox ? <Box h="200px" w="259px" pt="1px"><Img src={agerOrMartinator[9]} h="150px" w="150px" ml="60px" mt="30px"/></Box> : <Box p="25px 100px 25px 100px"><Text color="#1A2A33">o</Text></Box>}
             </Link>
           </Box>
         </Box>
         <Box mt="35px">
-          {!start ? <Button w="262px" bgColor="#65E9E4" h="65px" fontSize="50px" boxShadow="0 8px #118C87" _active={{ boxShadow: '0 8px #23a19d', transform: 'translateY(4px)' }} _hover={{ backgroundColor: '#26bbb6' }} onClick={() => setStart(true)}>Reset</Button> : <Button w="262px" bgColor="#FFC860" h="65px" fontSize="50px" boxShadow="0 8px #CC8B13" _active={{ boxShadow: '0 8px #9b680b', transform: 'translateY(4px)' }} _hover={{ backgroundColor: '#ffb01d' }} onClick={() => setStart(false)}>Start</Button>}
+          {!start ? <Button w="262px" bgColor="#65E9E4" h="65px" fontSize="50px" boxShadow="0 8px #118C87" _active={{ boxShadow: '0 8px #23a19d', transform: 'translateY(4px)' }} _hover={{ backgroundColor: '#26bbb6' }} onClick={() => { setStart(true); setReset(true) }}>Reset</Button> : <Button w="262px" bgColor="#FFC860" h="65px" fontSize="50px" boxShadow="0 8px #CC8B13" _active={{ boxShadow: '0 8px #9b680b', transform: 'translateY(4px)' }} _hover={{ backgroundColor: '#ffb01d' }} onClick={() => setStart(false)}>Start</Button>}
         </Box>
         <Box display="flex" justifyContent="center" mt="25px" alignItems="center">
           <Img src={tie} h="80px"/>
