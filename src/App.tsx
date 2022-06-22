07a00f66c3a434065bf59e8aafd5369d150cd59a
Noah Reiter <n.reiter@alpin11.com>
2022-06-22 15:55:51 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,72 +4,73 @@ import agerFarmer from './agerfarmer.png'
 import martinator from './martinator.png'
 import tie from './tie.png'
 import quack from './Quack.mp3'
+import { render } from 'react-dom'
 
 const App = () => {
   const sound = useMemo(() => new Audio(quack), [])
   const [start, setStart] = useState(true)
-  const [turn, setTurn] = useState(false)
   const [count, setCount] = useState(0)
   const [reset, setReset] = useState(false)
-  const agerOrMartinator = [
-    'o', 'o', 'o',
-    'o', 'o', 'o',
-    'o', 'o', 'o',
-  ]
 
-  const firstrow = [
-    'o', 'o', 'o',
-  ]
-  const secondrow = [
-    'o', 'o', 'o',
-  ]
-  const thirdrow = [
-    'o', 'o', 'o',
-  ]
+  const [firstRow, setFirstRow] = useState(['o', 'o', 'o'])
+  const [secondRow, setSecondRow] = useState(['o', 'o', 'o'])
+  const [thirdRow, setThirdRow] = useState(['o', 'o', 'o'])
 
   if (reset) {
     setStart(true)
     setCount(0)
-    setTurn(false)
     setReset(false)
+    setFirstRow(['o', 'o', 'o'])
+    setSecondRow(['o', 'o', 'o'])
+    setThirdRow(['o', 'o', 'o'])
   }
+  const [image, setImage] = useState(agerFarmer)
 
-  agerOrMartinator[0] = martinator
-  console.log(secondrow)
-
-  const round = 'x'
+  const [round, setRound] = useState(69)
 
   function updateIndex (index: number, row: number) {
+    if (count % 2 === 0) {
+      setImage(martinator)
+    } else if (count % 2 === 1) {
+      setImage(agerFarmer)
+    }
+    if (row === 0) {
+      firstRow.splice(index, 1, image)
+    }
     if (row === 1) {
-      firstrow.splice(index, 1, 'irgendwos')
+      secondRow.splice(index, 1, image)
     }
     if (row === 2) {
-      secondrow.splice(index, 1, 'irgendwos')
-    }
-    if (row === 3) {
-      thirdrow.splice(index, 1, 'irgendwos')
+      thirdRow.splice(index, 1, image)
     }
   }
+
+  if (round === 70) {
+    setStart(true)
+    setCount(0)
+    setReset(false)
+  }
+  console.log(count)
 
   return (
     <Container w="100vw" h="100vh" maxW="100vw" bgColor="#1A2A33" display="flex" flexDirection="row" padding="0" justifyContent="space-between">
       <Box w="33vw" justifyContent="center" alignItems="center" display="flex" gap="30px" flexDir="column">
-        <Text color="#31C3BD" fontSize="80px">Number</Text>
+        <Text color="#31C3BD" fontSize="80px">1</Text>
         <Img src={agerFarmer} alt="Ager Farmer" w="220px" h="220px"/>
       </Box>
       <Box textAlign="center">
         <Box>
           <Heading color="#B9CFF0" fontSize="150px" fontFamily="sans-serif" fontWeight={500} mt="75px">TicTacToe</Heading>
         </Box>
-        <Box display="flex" justifyContent="space-evenly" fontSize="80px" w="320px" ml="auto" mr="auto" mb="50px">
+        <Box display="flex" justifyContent="space-evenly" fontSize="80px" w="340px" ml="auto" mr="auto" mb="50px">
           <Text color="white">Round </Text>
           <Text color="#31C3BD">{round}</Text>
         </Box>
         <Box w="600px" color="grey" display="flex" fontSize="100px" justifyContent="space-evenly" ml="auto" mr="auto" >
           <Box>
-            {firstrow.map((item, index) => {
+            {firstRow.map((item, index) => {
               return (
-                <Link onClick={() => { setCount(count + 1); updateIndex(1, index) }} _hover={{ border: 'none' }} key={`${index}benjaminblümchentorte`}>
+                <Link _hover={{ border: 'none' }} key={`${index}benjaminblümchentorte`}>
                   {(item !== 'o')
                     ? (
                       <Box
@@ -81,15 +82,15 @@ const App = () => {
                       ><Img src={item} h="150px" w="150px" ml="60px" mt="30px"/>
                       </Box>
                       )
-                    : <Box borderRight="4px" borderBottom={index === 2 ? '' : '4px'} p="25px 100px 25px 100px"><Text color="#1A2A33">{item}</Text></Box>}
+                    : <Box borderRight="4px" borderBottom={index === 2 ? '' : '4px'} p="25px 100px 25px 100px" onClick={() => { setCount(count + 1); updateIndex(index, 0); setStart(false); sound.play() }}><Text color="#1A2A33">{item}</Text></Box>}
                 </Link>
               )
             })}
           </Box>
           <Box>
-            {secondrow.map((item, index) => {
+            {secondRow.map((item, index) => {
               return (
-                <Link onClick={() => { setCount(count + 1); secondrow[index] = 'x'; updateIndex(2, index) }} _hover={{ border: 'none' }} key={`${index}kinder`}>
+                <Link _hover={{ border: 'none' }} key={`${index}kinder`}>
                   {(item !== 'o')
                     ? (
                       <Box
@@ -101,15 +102,15 @@ const App = () => {
                       ><Img src={item} h="150px" w="150px" ml="60px" mt="30px"/>
                       </Box>
                       )
-                    : <Box borderRight="4px" borderBottom={index === 2 ? '' : '4px'} p="25px 100px 25px 100px"><Text color="#1A2A33">{item}</Text></Box>}
+                    : <Box borderRight="4px" borderBottom={index === 2 ? '' : '4px'} p="25px 100px 25px 100px" onClick={() => { setCount(count + 1); updateIndex(index, 1); setStart(false); sound.play() }}><Text color="#1A2A33">{item}</Text></Box>}
                 </Link>
               )
             })}
           </Box>
           <Box>
-            {thirdrow.map((item, index) => {
+            {thirdRow.map((item, index) => {
               return (
-                <Link onClick={() => { setCount(count + 1); updateIndex(3, index) }} _hover={{ border: 'none' }} key={`${index}kinderSalami`}>
+                <Link _hover={{ border: 'none' }} key={`${index}kinderSalami`}>
                   {(item !== 'o')
                     ? (
                       <Box
@@ -120,7 +121,7 @@ const App = () => {
                       ><Img src={item} h="150px" w="150px" ml="60px" mt="30px"/>
                       </Box>
                       )
-                    : <Box borderBottom={index === 2 ? '' : '4px'} p="25px 100px 25px 100px"><Text color="#1A2A33">{item}</Text></Box>}
+                    : <Box borderBottom={index === 2 ? '' : '4px'} p="25px 100px 25px 100px" onClick={() => { setCount(count + 1); updateIndex(index, 2); setStart(false); sound.play() }}><Text color="#1A2A33">{item}</Text></Box>}
                 </Link>
               )
             })}
@@ -131,11 +132,11 @@ const App = () => {
         </Box>
         <Box display="flex" justifyContent="center" mt="25px" alignItems="center">
           <Img src={tie} h="80px"/>
-          <Text color="#31C3BD" fontSize="80px">Number</Text>
+          <Text color="#31C3BD" fontSize="80px">8</Text>
         </Box>
       </Box>
       <Box w="33vw" justifyContent="center" alignItems="center" display="flex" gap="30px" flexDir="column">
-        <Text color="#31C3BD" fontSize="80px">Number</Text>
+        <Text color="#31C3BD" fontSize="80px">7</Text>
         <Img src={martinator} alt="Ager Farmer" h="220px"/>
       </Box>
     </Container>
