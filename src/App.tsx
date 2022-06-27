13805d285dc3f9f9678b8854b3d78abbe280886e
Noah Reiter <n.reiter@alpin11.com>
2022-06-27 08:47:08 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 // imports
 import { Box, Button, Container, Heading, Hide, Img, Link, Select, Text } from '@chakra-ui/react'
-import React, { useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import agerFarmer from './agerfarmer.png'
 import martinator from './martinator.png'
 import tie from './tie.png'
@@ -13,8 +13,6 @@ import max from './maxDerGroße.png'
 import maxTheQueen from './maxDieGroßeQueen.png'
 import david from './davidDerWildeHengst.png'
 import davidTheStalker from './daviDerBadHengst.png'
-import { useForm } from 'react-hook-form'
-import { getValueTransition } from 'framer-motion/types/animation/utils/transitions'
 
 // App component
 const App = () => {
@@ -30,6 +28,55 @@ const App = () => {
   const [turn, setTurn] = useState('')
   const [end, setEnd] = useState(false)
   const [valueLeftSide, setValue] = useState<string>()
+  let [characterLeftSide, setCharacterLeftSide] = useState(agerFarmer)
+  let [badLeftSide, setBadLeftSide] = useState(badAgerFarmer)
+  let [characterRightSide, setCharacterRightSide] = useState(martinator)
+  let [badRightSide, setBadRightSide] = useState(badMartinator)
+  const [valueRightSide, setValueRightSide] = useState<string>()
+
+  // handle the value of the select for the left side
+  if (valueLeftSide === 'Martinator') {
+    characterLeftSide = martinator
+    badLeftSide = badMartinator
+  }
+  if (valueLeftSide === 'AgerFarmer') {
+    characterLeftSide = agerFarmer
+    badLeftSide = badAgerFarmer
+  }
+  if (valueLeftSide === 'Max') {
+    characterLeftSide = max
+    badLeftSide = maxTheQueen
+  }
+  if (valueLeftSide === 'David') {
+    characterLeftSide = david
+    badLeftSide = davidTheStalker
+  }
+  if (valueLeftSide === 'Alex') {
+    characterLeftSide = alex
+    badLeftSide = badAlex
+  }
+
+  // handle the value of the select for the right side
+  if (valueRightSide === 'Martinator') {
+    characterRightSide = martinator
+    badRightSide = badMartinator
+  }
+  if (valueRightSide === 'AgerFarmer') {
+    characterRightSide = agerFarmer
+    badRightSide = badAgerFarmer
+  }
+  if (valueRightSide === 'Max') {
+    characterRightSide = max
+    badRightSide = maxTheQueen
+  }
+  if (valueRightSide === 'David') {
+    characterRightSide = david
+    badRightSide = davidTheStalker
+  }
+  if (valueRightSide === 'Alex') {
+    characterRightSide = alex
+    badRightSide = badAlex
+  }
 
   // added rows
   const [firstRow, setFirstRow] = useState(['o', 'o', 'o'])
@@ -40,22 +87,26 @@ const App = () => {
   const [finalEnd, setFinalEnd] = useState(false)
 
   // set image useState
-  const [image, setImage] = useState(agerFarmer)
+  const [image, setImage] = useState(characterLeftSide)
+  useEffect(() => {
+    setImage(characterLeftSide)
+  }
+  , [characterLeftSide])
 
   // added reset option
   if (reset) {
     setStart(true)
     setCount(0)
-    setReset(false)
     setFirstRow(['o', 'o', 'o'])
     setSecondRow(['o', 'o', 'o'])
     setThirdRow(['o', 'o', 'o'])
     setMartinatorCount(0)
     setAgerFarmerCount(0)
-    setImage(agerFarmer)
+    setImage(characterLeftSide)
     setTurn('')
     setEnd(false)
     setFinalEnd(false)
+    setReset(false)
   }
 
   // adding vars
@@ -68,13 +119,13 @@ const App = () => {
   function updateIndex (index: number, row: number) {
     if (count % 2 === 0) {
       if (martinatorCount !== 4) {
-        setImage(martinator)
+        setImage(characterRightSide)
         setMartinatorCount(martinatorCount + 1)
         setTurn('martinator')
       } else setEnd(true)
     } else if (count % 2 !== 0) {
       if (agerFarmerCount !== 5) {
-        setImage(agerFarmer)
+        setImage(characterLeftSide)
         setAgerFarmerCount(agerFarmerCount + 1)
         setTurn('agerFarmer')
       } else setEnd(true)
@@ -102,7 +153,7 @@ const App = () => {
 
   // tie function
   function tieTie () {
-    if (firstRow[0] === badAgerFarmer || firstRow[1] === badAgerFarmer || firstRow[2] === badAgerFarmer || secondRow[0] === badMartinator || secondRow[1] === badMartinator || secondRow[2] === badMartinator || thirdRow[0] === badAgerFarmer || thirdRow[1] === badAgerFarmer || thirdRow[2] === badAgerFarmer || firstRow[0] === badMartinator || firstRow[1] === badMartinator || firstRow[2] === badMartinator || secondRow[0] === badAgerFarmer || secondRow[1] === badAgerFarmer || secondRow[2] === badAgerFarmer || thirdRow[0] === badMartinator || thirdRow[1] === badMartinator || thirdRow[2] === badMartinator) {
+    if (firstRow[0] === badLeftSide || firstRow[1] === badLeftSide || firstRow[2] === badLeftSide || secondRow[0] === badRightSide || secondRow[1] === badRightSide || secondRow[2] === badRightSide || thirdRow[0] === badLeftSide || thirdRow[1] === badLeftSide || thirdRow[2] === badLeftSide || firstRow[0] === badRightSide || firstRow[1] === badRightSide || firstRow[2] === badRightSide || secondRow[0] === badLeftSide || secondRow[1] === badLeftSide || secondRow[2] === badLeftSide || thirdRow[0] === badRightSide || thirdRow[1] === badRightSide || thirdRow[2] === badRightSide) {
       return null
     } else {
       setTieCount(tieCount + 1)
@@ -110,90 +161,90 @@ const App = () => {
   }
 
   // if statement for ending game
-  if (firstRow[0] === agerFarmer && firstRow[1] === agerFarmer && firstRow[2] === agerFarmer) {
+  if (firstRow[0] === characterLeftSide && firstRow[1] === characterLeftSide && firstRow[2] === characterLeftSide) {
     setEnd(true)
     agerFarmerWin()
-    setFirstRow([badAgerFarmer, badAgerFarmer, badAgerFarmer])
-  } else if (secondRow[0] === agerFarmer && secondRow[1] === agerFarmer && secondRow[2] === agerFarmer) {
-    setSecondRow([badAgerFarmer, badAgerFarmer, badAgerFarmer])
+    setFirstRow([badLeftSide, badLeftSide, badLeftSide])
+  } else if (secondRow[0] === characterLeftSide && secondRow[1] === characterLeftSide && secondRow[2] === characterLeftSide) {
+    setSecondRow([badLeftSide, badLeftSide, badLeftSide])
     agerFarmerWin()
     setEnd(true)
-  } else if (thirdRow[0] === agerFarmer && thirdRow[1] === agerFarmer && thirdRow[2] === agerFarmer) {
-    setThirdRow([badAgerFarmer, badAgerFarmer, badAgerFarmer])
+  } else if (thirdRow[0] === characterLeftSide && thirdRow[1] === characterLeftSide && thirdRow[2] === characterLeftSide) {
+    setThirdRow([badLeftSide, badLeftSide, badLeftSide])
     agerFarmerWin()
     setEnd(true)
-  } else if (firstRow[0] === martinator && firstRow[1] === martinator && firstRow[2] === martinator) {
-    setFirstRow([badMartinator, badMartinator, badMartinator])
+  } else if (firstRow[0] === characterRightSide && firstRow[1] === characterRightSide && firstRow[2] === characterRightSide) {
+    setFirstRow([badRightSide, badRightSide, badRightSide])
     martinWin()
     setEnd(true)
-  } else if (secondRow[0] === martinator && secondRow[1] === martinator && secondRow[2] === martinator) {
-    setSecondRow([badMartinator, badMartinator, badMartinator])
+  } else if (secondRow[0] === characterRightSide && secondRow[1] === characterRightSide && secondRow[2] === characterRightSide) {
+    setSecondRow([badRightSide, badRightSide, badRightSide])
     martinWin()
     setEnd(true)
-  } else if (thirdRow[0] === martinator && thirdRow[1] === martinator && thirdRow[2] === martinator) {
-    setThirdRow([badMartinator, badMartinator, badMartinator])
+  } else if (thirdRow[0] === characterRightSide && thirdRow[1] === characterRightSide && thirdRow[2] === characterRightSide) {
+    setThirdRow([badRightSide, badRightSide, badRightSide])
     martinWin()
     setEnd(true)
-  } else if (firstRow[0] === agerFarmer && secondRow[0] === agerFarmer && thirdRow[0] === agerFarmer) {
-    setFirstRow([badAgerFarmer, firstRow[1], firstRow[2]])
-    setSecondRow([badAgerFarmer, secondRow[1], secondRow[2]])
-    setThirdRow([badAgerFarmer, thirdRow[1], thirdRow[2]])
+  } else if (firstRow[0] === characterLeftSide && secondRow[0] === characterLeftSide && thirdRow[0] === characterLeftSide) {
+    setFirstRow([badLeftSide, firstRow[1], firstRow[2]])
+    setSecondRow([badLeftSide, secondRow[1], secondRow[2]])
+    setThirdRow([badLeftSide, thirdRow[1], thirdRow[2]])
     agerFarmerWin()
     setEnd(true)
-  } else if (firstRow[1] === agerFarmer && secondRow[1] === agerFarmer && thirdRow[1] === agerFarmer) {
-    setFirstRow([firstRow[0], badAgerFarmer, firstRow[2]])
-    setSecondRow([secondRow[0], badAgerFarmer, secondRow[2]])
-    setThirdRow([thirdRow[0], badAgerFarmer, thirdRow[2]])
+  } else if (firstRow[1] === characterLeftSide && secondRow[1] === characterLeftSide && thirdRow[1] === characterLeftSide) {
+    setFirstRow([firstRow[0], badLeftSide, firstRow[2]])
+    setSecondRow([secondRow[0], badLeftSide, secondRow[2]])
+    setThirdRow([thirdRow[0], badLeftSide, thirdRow[2]])
     agerFarmerWin()
     setEnd(true)
-  } else if (firstRow[2] === agerFarmer && secondRow[2] === agerFarmer && thirdRow[2] === agerFarmer) {
-    setFirstRow([firstRow[0], firstRow[1], badAgerFarmer])
-    setSecondRow([secondRow[0], secondRow[1], badAgerFarmer])
-    setThirdRow([thirdRow[0], thirdRow[1], badAgerFarmer])
+  } else if (firstRow[2] === characterLeftSide && secondRow[2] === characterLeftSide && thirdRow[2] === characterLeftSide) {
+    setFirstRow([firstRow[0], firstRow[1], badLeftSide])
+    setSecondRow([secondRow[0], secondRow[1], badLeftSide])
+    setThirdRow([thirdRow[0], thirdRow[1], badLeftSide])
     agerFarmerWin()
     setEnd(true)
-  } else if (firstRow[0] === martinator && secondRow[0] === martinator && thirdRow[0] === martinator) {
-    setFirstRow([badMartinator, firstRow[1], firstRow[2]])
-    setSecondRow([badMartinator, secondRow[1], secondRow[2]])
-    setThirdRow([badMartinator, thirdRow[1], thirdRow[2]])
+  } else if (firstRow[0] === characterRightSide && secondRow[0] === characterRightSide && thirdRow[0] === characterRightSide) {
+    setFirstRow([badRightSide, firstRow[1], firstRow[2]])
+    setSecondRow([badRightSide, secondRow[1], secondRow[2]])
+    setThirdRow([badRightSide, thirdRow[1], thirdRow[2]])
     martinWin()
     setEnd(true)
-  } else if (firstRow[1] === martinator && secondRow[1] === martinator && thirdRow[1] === martinator) {
-    setFirstRow([firstRow[0], badMartinator, firstRow[2]])
-    setSecondRow([secondRow[0], badMartinator, secondRow[2]])
-    setThirdRow([thirdRow[0], badMartinator, thirdRow[2]])
+  } else if (firstRow[1] === characterRightSide && secondRow[1] === characterRightSide && thirdRow[1] === characterRightSide) {
+    setFirstRow([firstRow[0], badRightSide, firstRow[2]])
+    setSecondRow([secondRow[0], badRightSide, secondRow[2]])
+    setThirdRow([thirdRow[0], badRightSide, thirdRow[2]])
     agerFarmerWin()
     setEnd(true)
     martinWin()
     setEnd(true)
-  } else if (firstRow[2] === martinator && secondRow[2] === martinator && thirdRow[2] === martinator) {
-    setFirstRow([firstRow[0], firstRow[1], badMartinator])
-    setSecondRow([secondRow[0], secondRow[1], badMartinator])
-    setThirdRow([thirdRow[0], thirdRow[1], badMartinator])
+  } else if (firstRow[2] === characterRightSide && secondRow[2] === characterRightSide && thirdRow[2] === characterRightSide) {
+    setFirstRow([firstRow[0], firstRow[1], badRightSide])
+    setSecondRow([secondRow[0], secondRow[1], badRightSide])
+    setThirdRow([thirdRow[0], thirdRow[1], badRightSide])
     martinWin()
     setEnd(true)
-  } else if (firstRow[0] === agerFarmer && secondRow[1] === agerFarmer && thirdRow[2] === agerFarmer) {
-    setFirstRow([badAgerFarmer, firstRow[1], firstRow[2]])
-    setSecondRow([secondRow[0], badAgerFarmer, secondRow[2]])
-    setThirdRow([thirdRow[0], thirdRow[1], badAgerFarmer])
+  } else if (firstRow[0] === characterLeftSide && secondRow[1] === characterLeftSide && thirdRow[2] === characterLeftSide) {
+    setFirstRow([badLeftSide, firstRow[1], firstRow[2]])
+    setSecondRow([secondRow[0], badLeftSide, secondRow[2]])
+    setThirdRow([thirdRow[0], thirdRow[1], badLeftSide])
     agerFarmerWin()
     setEnd(true)
-  } else if (firstRow[2] === agerFarmer && secondRow[1] === agerFarmer && thirdRow[0] === agerFarmer) {
-    setFirstRow([firstRow[0], firstRow[1], badAgerFarmer])
-    setSecondRow([secondRow[0], badAgerFarmer, secondRow[2]])
-    setThirdRow([badAgerFarmer, thirdRow[1], thirdRow[2]])
+  } else if (firstRow[2] === characterLeftSide && secondRow[1] === characterLeftSide && thirdRow[0] === characterLeftSide) {
+    setFirstRow([firstRow[0], firstRow[1], badLeftSide])
+    setSecondRow([secondRow[0], badLeftSide, secondRow[2]])
+    setThirdRow([badLeftSide, thirdRow[1], thirdRow[2]])
     agerFarmerWin()
     setEnd(true)
-  } else if (firstRow[0] === martinator && secondRow[1] === martinator && thirdRow[2] === martinator) {
-    setFirstRow([badMartinator, firstRow[1], firstRow[2]])
-    setSecondRow([secondRow[0], badMartinator, secondRow[2]])
-    setThirdRow([thirdRow[0], thirdRow[1], badMartinator])
+  } else if (firstRow[0] === characterRightSide && secondRow[1] === characterRightSide && thirdRow[2] === characterRightSide) {
+    setFirstRow([badRightSide, firstRow[1], firstRow[2]])
+    setSecondRow([secondRow[0], badRightSide, secondRow[2]])
+    setThirdRow([thirdRow[0], thirdRow[1], badRightSide])
     martinWin()
     setEnd(true)
-  } else if (firstRow[2] === martinator && secondRow[1] === martinator && thirdRow[0] === martinator) {
-    setFirstRow([firstRow[0], firstRow[1], badMartinator])
-    setSecondRow([secondRow[0], badMartinator, secondRow[2]])
-    setThirdRow([badMartinator, thirdRow[1], thirdRow[2]])
+  } else if (firstRow[2] === characterRightSide && secondRow[1] === characterRightSide && thirdRow[0] === characterRightSide) {
+    setFirstRow([firstRow[0], firstRow[1], badRightSide])
+    setSecondRow([secondRow[0], badRightSide, secondRow[2]])
+    setThirdRow([badRightSide, thirdRow[1], thirdRow[2]])
     martinWin()
     setEnd(true)
   } else if (firstRow[0] !== 'x' && firstRow[1] !== 'x' && firstRow[2] !== 'x' && secondRow[0] !== 'x' && secondRow[1] !== 'x' && secondRow[2] !== 'x' && thirdRow[0] !== 'x' && thirdRow[1] !== 'x' && thirdRow[2] !== 'x' && end) {
@@ -212,17 +263,17 @@ const App = () => {
   function showAgerFarmer () {
     if (turn === 'martinator') {
       return (
-        <Img src={agerFarmer} alt="Ager Farmer" w="220px" h="220px" filter="grayscale(100%)"/>
+        <Img src={characterLeftSide} alt="Ager Farmer" w="220px" h="220px" filter="grayscale(100%)"/>
       )
     }
     if (turn === '') {
       return (
-        <Img src={agerFarmer} alt="Ager Farmer" w="220px" h="220px"/>
+        <Img src={characterLeftSide} alt="Ager Farmer" w="220px" h="220px"/>
       )
     }
     if (turn === 'agerFarmer') {
       return (
-        <Img src={agerFarmer} alt="Ager Farmer" w="220px" h="220px"/>
+        <Img src={characterLeftSide} alt="Ager Farmer" w="220px" h="220px"/>
       )
     }
   }
@@ -231,17 +282,17 @@ const App = () => {
   function showMartinator () {
     if (turn === 'agerFarmer') {
       return (
-        <Img src={martinator} alt="Ager Farmer" w="180px" h="220px" filter="grayscale(100%)"/>
+        <Img src={characterRightSide} alt="Ager Farmer" w="180px" h="220px" filter="grayscale(100%)"/>
       )
     }
     if (turn === '') {
       return (
-        <Img src={martinator} alt="Martinator" w="180px" h="220px"/>
+        <Img src={characterRightSide} alt="Martinator" w="180px" h="220px"/>
       )
     }
     if (turn === 'martinator') {
       return (
-        <Img src={martinator} alt="Martinator" w="180px" h="220px"/>
+        <Img src={characterRightSide} alt="Martinator" w="180px" h="220px"/>
       )
     }
   }
@@ -250,12 +301,12 @@ const App = () => {
   function itsYourTurn () {
     if (turn === 'agerFarmer') {
       return (
-        <Text color="white" fontSize={['18px', '25px', '35px', '50px']} mb="50px">AgerFarmer it's your turn!</Text>
+        <Text color="white" fontSize={['18px', '25px', '35px', '50px']} mb="50px">{valueLeftSide} it's your turn!</Text>
       )
     }
     if (turn === 'martinator') {
       return (
-        <Text color="white" fontSize={['18px', '25px', '35px', '50px']} mb="50px">Martinator it's your turn!</Text>
+        <Text color="white" fontSize={['18px', '25px', '35px', '50px']} mb="50px">{valueRightSide} it's your turn!</Text>
       )
     }
     if (turn === '') {
@@ -265,17 +316,23 @@ const App = () => {
     }
   }
 
+  // feature that you cant use the same character twice
+  if (characterLeftSide === characterRightSide) {
+    setValueRightSide('Martinator')
+    setValue('AgerFarmer')
+  }
+
   // return
   return (
     <Container w="100vw" h="100vh" maxW="100vw" bgColor="#1A2A33" display="flex" flexDirection="row" padding="0" justifyContent="space-between">
       <Hide below="xl">
         <Box w="33vw" justifyContent="center" alignItems="center" display="flex" flexDir="column">
           <Select placeholder="Choose your Player" color="#1A2A33" w="300px" textAlign="center" bgColor="#B9CFF0" value={valueLeftSide} onChange={e => setValue(e.target.value)}>
-            <option value="agerFarmer">The Farmer</option>
-            <option value="martinator">The Stoner</option>
-            <option value="max">The Queen</option>
-            <option value="alex">The Gay Punk</option>
-            <option value="david">The Stalker</option>
+            <option value="AgerFarmer">The Farmer</option>
+            <option value="Martinator">The Stoner</option>
+            <option value="Max">The Queen</option>
+            <option value="Alex">The Gay Punk</option>
+            <option value="David">The Stalker</option>
           </Select>
           <Text color="#31C3BD" fontSize="80px" mb="30px">{agerWinCount}</Text>
           {showAgerFarmer()}
@@ -363,12 +420,12 @@ const App = () => {
       </Box>
       <Hide below="xl">
         <Box w="33vw" justifyContent="center" alignItems="center" display="flex" flexDir="column">
-          <Select placeholder="Choose your Player" color="#1A2A33" w="300px" textAlign="center" bgColor="#B9CFF0">
-            <option value="agerFarmer">The Farmer</option>
-            <option value="martinator">The Stoner</option>
-            <option value="max">The Queen</option>
-            <option value="alex">The Gay Punk</option>
-            <option value="david">The Stalker</option>
+          <Select placeholder="Choose your Player" color="#1A2A33" w="300px" textAlign="center" bgColor="#B9CFF0" value={valueRightSide} onChange={e => setValueRightSide(e.target.value)}>
+            <option value="AgerFarmer">The Farmer</option>
+            <option value="Martinator">The Stoner</option>
+            <option value="Max">The Queen</option>
+            <option value="Alex">The Gay Punk</option>
+            <option value="David">The Stalker</option>
           </Select>
           <Text color="#31C3BD" fontSize="80px" mb="30px">{martinatorWinCount}</Text>
           {showMartinator()}
